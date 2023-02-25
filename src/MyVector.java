776e4cd1046f5c809ccb532ab99b0b989011c828
ryudb0 <ryudb030@gmail.com>
2023-02-25 16:27:32 +0900

    ArrayList LinkedList

--- src/MyVector.java
+++ src/MyVector.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.function.UnaryOperator;
 
 public class MyVector implements List {
 
@@ -51,5 +52,65 @@ public class MyVector implements List {
 		size--;
 		return oldObj;
 	}
+	
+	public boolean remove(Object obj) {
+		for(int i=0; i<size; i++) {
+			if(obj.equals(data[i])) {
+				remove(i);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void trimTiSize() {
+		setCapacity(size);
+	}
+	
+	private void setCapacity(int capacity) {
+		if(this.capacity == capacity) return;
+		
+		Object[] tmp = new Object[capacity];
+		System.arraycopy(data, 0, tmp, 0, size);
+		data = tmp;
+		this.capacity = capacity;
+	}
+	
+	public void clear() {
+		for(int i=0; i<size; i++) {
+			data[i] = null;
+		}
+		size = 0;
+	}
+	
+	public Object[] toArray() {
+		Object[] result = new Object[size];
+		System.arraycopy(data, 0, result, 0, size);
+		return result;
+	}
+	public boolean isEmpty() { return size ==0;}
+	public int capacity() { return capacity;}
+	public int size() { return size;}
+	
+	public boolean contains(Object o) { return false;}
+	public Iterator iterator() { return null;}
+	public Object[] toArray(Object a[]) { return null;}
+	public boolean containsAll(Collection c) { return false;}
+	public boolean addAll(Collection c) { return false; }
+	public boolean addAll(int index, Collection c) { return false;}
+	public boolean removeAll(Collection c) { return false;}
+	public boolean retainAll(Collection c) { return false;}
+	public boolean equals(Object o) { return false;}
+	public Object set(int index, Object element) { return null;}
+	public void add(int index, Object element) { }
+	public int indexOf(Object o) { return -1;} 
+	public int lastIndexOf(Object o) { return -1;}
+	public ListIterator listIterator() { return null;}
+	public ListIterator listIterator(int index) { return null;}
+	public List subList(int fromIndex, int toIndex) { return null;}
+	
+//	default void sort(Comparator<T> c) { /* 내용 생략 */} 
+//	default Spliterator splilterator() { /* void */}
+//	default void replaceAll(UnaryOperator operator) { /* void */}
 	
 }
